fa6d4eed8345b4080088be9b7ab52daf8b1fcfb8
vazquez-andres <andres_vazquezocampo@outlook.es>
2022-06-28 16:43:48 -0500

    fix pdf

--- controlador/reporte_empleado.php
+++ controlador/reporte_empleado.php
@@ -1,17 +1,18 @@
 <?php
 require('fpdf184/fpdf.php');
-
-include 'conexion_bd.php';
 class PDF extends FPDF
 {
-// Page header
+/* Page header */
 function Header()
 {
-    // Logo
+    
     $this->Image('../assets/images/reporte.png',15,8,70);
     // Arial bold 15
     $this->SetFont('Arial','B',15);
     // Move to the right
+    $this->Cell(80);
+    // Title
+    $this->Cell(121,15,'REPORTE SEMANAL DE VENTAS',0,0,'C');
 
     // Line break
     $this->Ln(8);
@@ -27,133 +28,92 @@ function Header()
     $meses = array("Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre");
     $dias = array("01","02","03","04","05","06","07","08","09","10","11","12","13","14","15","16","17","18","19","20","21","22","23","24","25","26","27","28","29","30","31");
     $this->Cell(315,-23, $diassemana[date('w')]." ".$dias[date('d')-1]." de ".$meses[date('n')-1]. " del ".date('Y'),0,0,'C');
-
-    $this->Ln(0);
-
-    $this->Cell(40);
+    $this->Ln(20);
     $this->SetFont('Arial','B',15);
-   
-    $this->Cell(121,15,'RECIBO DE PAGO',0,0,'C');
-    
     // Title
     $this->Cell(125,15,'',0,0,'C');
+    
 }
-
-// Page footer
+/* Page footer */
 function Footer()
 {
-    // Position at 1.5 cm from bottom
+    /* Position at 1.5 cm from bottom */
     $this->SetY(-15);
-    // Arial italic 8
+    /* Arial italic 8 */
     $this->SetFont('Arial','I',8);
-    // Page number
+    /* Page number */
     $this->Cell(0,10,'Page '.$this->PageNo().'/{nb}',0,0,'C');
 }
 }
 
-$usuario=$_POST['nombre'];
-
-$consulta="select producto,precio,nombre_vendedor,fecha from ventas where nombre_vendedor='".$usuario."'";
-$result=mysqli_query($conexion,$consulta);
-$number_of_products = mysqli_num_rows($result);
-
-//Initialize the 3 columns and the total
-$column_code = "";
-$column_name = "";
-$column_fecha = "";
-$column_price = "";
-
-$total = 0;
-
-//For each row, add the field to the corresponding column
-while($row = mysqli_fetch_array($result))
-{
-    $code = $row["producto"];
-    $name = substr($row["nombre_vendedor"],0,20);
-    $real_price = $row["precio"];
-    
-
-    $fecha = $row["fecha"];
-    $column_code = $column_code.$code."\n";
-    $column_name = $column_name.$name."\n";
-    $column_fecha = $column_fecha.$fecha."\n";
-    $column_price = $column_price.'$ '.$real_price*0.40."\n";
-
-    //Sum all the Prices (TOTAL)
-    $total = $total+$real_price;
-}
-mysqli_close($conexion);
-
-// Instanciation of inherited class
+/* Instanciation of inherited class */
 $pdf = new PDF();
 $pdf->AliasNbPages();
 $pdf->AddPage();
 $pdf->SetFont('Times','',12);
+$pdf->SetY(40);
+
+require "config.php";//connection to database
+//SQL to get 10 records
+$sql="select * from ventas";
+$sql2="SELECT SUM(precio) from ventas";
+//SELECT SUM(precio)*0.40 from ventas;
+$sql3="SELECT COUNT(id_venta) from ventas";
 
 
 
-//Fields Name position
-$Y_Fields_Name_position =60;
-//Table position, under Fields Name
-$Y_Table_Position = 66;
-
-//First create each Field Name
-//Gray color filling each Field Name box
-$pdf->SetFillColor(232,232,232);
-//Bold Font for Field Name
+$width_cell=array(40,40,70,40,95,95,31);
 $pdf->SetFont('Arial','B',12);
-$pdf->SetY($Y_Fields_Name_position);
-$pdf->SetX(15);
-$pdf->Cell(50,6,'PRODUCTO/SERVICIO',1,0,'L',1);
-$pdf->SetX(65);
-$pdf->Cell(30,6,'BARBERO',1,0,'L',1);
-$pdf->SetX(95);
-$pdf->Cell(90,6,'FECHA',1,0,'L',1);
+//Background color of header//
+$pdf->SetFillColor(193,229,252);
 
-$pdf->SetX(155);
-$pdf->Cell(38,6,'TOTAL',1,0,'R',1);
-$pdf->Ln();
-
-//Now show the 3 columns
-$pdf->SetFont('Arial','',12);
-$pdf->SetY($Y_Table_Position);
-$pdf->SetX(15);
-$pdf->MultiCell(50,6,$column_code,1);
-$pdf->SetY($Y_Table_Position);
-
-$pdf->SetX(65);
-$pdf->MultiCell(30,6,$column_name,1);
-$pdf->SetY($Y_Table_Position);
-
-$pdf->SetX(95);
-$pdf->MultiCell(60,6,$column_fecha,1);
-$pdf->SetY($Y_Table_Position);
-
-$pdf->SetX(155);
-$pdf->MultiCell(38,6,$column_price,1,'R');
+// Header starts /// 
+//First header column //
+$pdf->Cell($width_cell[0],10,'PRODUCTO',2,0,'C',true);
+//Second header column//
+$pdf->Cell($width_cell[1],10,'BARBERO',2,0,'C',true);
+//Third header column//
+$pdf->Cell($width_cell[2],10,'FECHA',2,0,'C',true); 
+//Fourth header column//
+$pdf->Cell($width_cell[3],10,'TOTAL',2,1,'C',true);
 
 
+$pdf->SetFont('Arial','',10);
+//Background color of header//
+$pdf->SetFillColor(235,236,236); 
+//to give alternate background fill color to rows// 
+$fill=false;
 
-$pdf->SetX(15);
-$pdf->MultiCell(178,8,'$ '.($total*0.40),1,'R');
+/// each record is one row  ///
+foreach ($dbo->query($sql) as $row) {
+$pdf->Cell($width_cell[0],7,$row['producto'],2,0,'C',$fill);
+$pdf->Cell($width_cell[1],7,$row['nombre_vendedor'],2,0,'C',$fill);
+$pdf->Cell($width_cell[2],7,$row['fecha'],2,0,'C',$fill);
+$precio_empleado=$row['precio'];
 
+$pdf->Cell($width_cell[3],7,"$ ".$row['precio'],2,1,'C',$fill);
+//to give alternate background fill  color to rows//
+$fill = !$fill;
 
-//Create lines (boxes) for each ROW (Product)
-//If you don't use the following code, you don't create the lines separating each row
-$i = 0;
-$pdf->SetY($Y_Table_Position);
-while ($i < $number_of_products)
-{
-    $pdf->SetX(15);
-    $pdf->MultiCell(178,6,'',1);
-    $i = $i +1;
 }
-$pdf->Ln(3);
+foreach ($dbo->query($sql2) as $row) {
+    $pdf->SetX(10);
+    $resultado=$row['SUM(precio)'];
+    $f = new NumberFormatter("es", NumberFormatter::SPELLOUT);
+    $pdf->Cell($width_cell[4],7,'TOTAL: ',2,0,'R',true);
+    $pdf->Cell($width_cell[5],7,"$ ".($resultado*0.40)." (".(mb_strtoupper($f->format($resultado*0.40)))." PESOS) MXN.",2,0,'L',$fill);
+    //to give alternate background fill  color to rows//
+    $fill = !$fill; 
+    }
+foreach ($dbo->query($sql3) as $row) {
+    $pdf->Ln();
 
-$f = new NumberFormatter("es", NumberFormatter::SPELLOUT);
-$pdf->MultiCell(129,3," (".(mb_strtoupper($f->format($total*0.40)))." PESOS) MXN.",0,'R');
-
-
-$pdf->Output();
+    $pdf->SetX(10);
+    setlocale(LC_MONETARY, 'es_MX');
+    $resultado=$row['COUNT(id_venta)'];
+    $pdf->Cell($width_cell[5],7,"MOSTRANDO ".$resultado." REGISTROS",2,0,'L',$fill);
+    $fill = !$fill; 
+    }    
 $pdf->Output();
 ?>
+     
