0be63213eba2740c55eaebbb8b1fd4d36626af5d
andres.vazquez.ocampo.contractor <andres.vazquez.ocampo.contractor@bbva.com>
2022-12-01 19:29:45 -0600

    encript password

--- index_admin.php
+++ index_admin.php
@@ -133,68 +133,13 @@ if($filas['id_cargo']=='1'){ //administrador
                                         </span>
                                     </button>
                                 </span>
-                            </div>    <div class="scrollbar-sidebar">
-                                <div class="app-sidebar__inner">
-                                    <ul class="vertical-nav-menu">
-                                        <li class="app-sidebar__heading">INICIO</li>
-                                        <li>
-                                            <a href="index_admin.php" class="mm-active">
-                                                <i class="metismenu-icon pe-7s-home"></i>
-                                                Dashboard
-                                            </a>
-                                        </li>
-                                        <li class="app-sidebar__heading">Ventas</li>
-                                        <li>
+                            </div>    
+                            
+                            <?php 
+                                include('vistas/main_menu.php'); 
 
-                                            <a href="ventas.php" >
-                                                <i class="metismenu-icon pe-7s-cart"></i>
-                                                Vender
-                                            </a>
-                                        </li>
-                                        <li class="app-sidebar__heading">Reportes</li>
-                                        <li>
+                            ?> 
 
-                                            <a href="reportes.php" >
-                                                <i class="metismenu-icon pe-7s-id"></i>
-                                                Generar Reportes
-                                            </a>
-                                        </li>
-                                        <li class="app-sidebar__heading">Usuarios</li>
-                                        <li>
-                                            <a href="agregar_usuarios.php">
-                                                <i class="metismenu-icon pe-7s-add-user"></i>
-                                                Agregar Usuarios
-                                            </a>
-                                        </li>
-                                        <li>
-                                            <a href="restablecer_password.php">
-                                                <i class="metismenu-icon pe-7s-key"></i>
-                                                Restablecer Password
-                                            </a>
-                                        </li>
-                                        <li class="app-sidebar__heading">Stock</li>
-                                        <li>
-                                            <a href="nuevo_stock.php">
-                                                <i class="metismenu-icon pe-7s-upload"></i>
-                                                Agregar Stock
-                                            </a>
-                                        </li>
-                                        <li>
-                                            <a href="modificar_stock.php">
-                                                <i class="metismenu-icon pe-7s-refresh"></i>
-                                                Modificar Stock
-                                            </a>
-                                        </li>
-                                        <li class="app-sidebar__heading">SEGURIDAD</li>
-                                        <li>
-                                            <a href="miHome.php">
-                                                <i class="metismenu-icon pe-7s-camera"></i>
-                                                Xiaomi Mi Home App
-                                            </a>
-                                        </li>
-                                    </ul>
-                                </div>
-                            </div>
                         </div>    <div class="app-main__outer">
                             <div class="app-main__inner">
                                 <div class="app-page-title">
