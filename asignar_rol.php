e86b4dec58676a6ee681696d0febcd4791bc895e
andres.vazquez.ocampo.contractor <andres.vazquez.ocampo.contractor@bbva.com>
2022-12-02 00:27:50 -0600

    new secction role

--- asignar_rol.php
+++ asignar_rol.php
@@ -167,21 +167,7 @@ if($filas['id_cargo']=='1'){ //administrador
                                                     <input name="correo" id="correo" placeholder="Correo@ejemplo.com" type="email" class="form-control">
                                                     </div>
                                                 </div> 
-                                                <div class="col-md-3">
-                                                    <div class="position-relative form-group">
-                                                   <input name="password" id="password" placeholder="contraseña" type="password" class="form-control">
-                                                    </div>
-                                                </div> 
-                                                 <div class="col-md-3">
-                                                    <div class="position-relative form-group">
-                                                    <select class="form-control" id="puesto">
-                                                    <option>Puesto</option>
-                                                    <option value="1">Administrador</option>
-                                                    <option value="2">Ventas</option>
-                                                    
-                                                </select>
-                                                    </div>
-                                                </div>  
+                                                   
                                             </div>
                                              <input type="button" class="mt-2 btn btn-success" value="Guardar" onclick="agregar_usuario()">
 
