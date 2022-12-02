32f60b861d3c58ce233bdf262816bb6eebf2c396
andres.vazquez.ocampo.contractor <andres.vazquez.ocampo.contractor@bbva.com>
2022-12-01 19:23:11 -0600

    encript password

--- controlador/agregar_usuario.php
+++ controlador/agregar_usuario.php
@@ -2,13 +2,10 @@
 
 include('conexion.php');
 $pdo = connect();
-// agregar alumnos
 try {
 	$sql = "CALL sp_agregarUsuario(:usuario, :correo, :password, :puesto); ";
 	$query = $pdo->prepare($sql);
-	$encriptar = $_POST['password'];
 	$encriptado = hash("sha256",$_POST['password']);
-	echo $encriptado;
 	$query->bindParam(':usuario', $_POST['usuario'], PDO::PARAM_STR);
 	$query->bindParam(':correo', $_POST['correo'], PDO::PARAM_STR);
 	$query->bindParam(':password', $encriptado , PDO::PARAM_STR);
@@ -18,6 +15,5 @@ try {
 	echo 'PDOException : '.  $e->getMessage();
 }
 
-// Lista de alumnos a ver
 include('../vistas/lista_usuarios.php');
 ?>
