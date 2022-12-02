7a499496aae6bda6163b733d0d415f211bfc75d5
andres.vazquez.ocampo.contractor <andres.vazquez.ocampo.contractor@bbva.com>
2022-12-02 00:23:02 -0600

    new secction role

--- controlador/asignar.php
+++ controlador/asignar.php
@@ -8,11 +8,12 @@ try {
 	$query->bindParam(':usuario', $_POST['usuario'], PDO::PARAM_STR);
     if($cargo=='Administrador'){
         $cargo=1;
+        echo $cargo;
     }
     else{
         $cargo=2;
+        echo $cargo;
     }
-
 	$query->bindParam(':cargo', $cargo, PDO::PARAM_STR);
 	$query->execute();
 } catch (PDOException $e) {
