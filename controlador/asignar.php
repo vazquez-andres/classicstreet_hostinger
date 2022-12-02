9ee490aa1675bd5249219a53edd482fb44e0d56f
andres.vazquez.ocampo.contractor <andres.vazquez.ocampo.contractor@bbva.com>
2022-12-02 00:32:22 -0600

    new secction role

--- controlador/asignar.php
+++ controlador/asignar.php
@@ -7,12 +7,7 @@ try {
 	$sql = "CALL sp_asingarRol(:usuario, :cargo); ";
 	$query = $pdo->prepare($sql);
 	$query->bindParam(':usuario', $_POST['usuario'], PDO::PARAM_STR);
-    if($cargo=='Administrador'){
-        $cargo=1;
-    }
-    else{
-        $cargo=2;
-    }
+ 
 
 	$query->bindParam(':cargo', $cargo, PDO::PARAM_STR);
 	$query->execute();
