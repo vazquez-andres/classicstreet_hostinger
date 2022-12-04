9c1ddeb9fabb51d6d2d35dc8de323a22e57a48e4
andres.vazquez.ocampo.contractor <andres.vazquez.ocampo.contractor@bbva.com>
2022-12-03 23:51:43 -0600

    add new sql backup

---
+++ controlador/backup_database/regresar_en_el_tiempo.php
@@ -0,0 +1,45 @@
+
+<?php
+try {
+
+    $filename = basename($_FILES["fileToUpload"]["name"]);
+    $templine = '';
+    $usuario = 'u917997591_andres';
+    $contraseña = 'CSBS2022db';
+    $mbd = new PDO('mysql:host=localhost;dbname=u917997591_classic_street', $usuario, $contraseña);
+
+    $fp = fopen($filename, 'r');
+   
+    // Loop through each line
+    while (($line = fgets($fp)) !== false) {
+        // Skip it if it's a comment
+        if (substr($line, 0, 2) == '--' || $line == ''){
+            continue;
+            echo $line;
+        }
+        // Add this line to the current segment drop table usuarios; drop table ventas; drop table cargo; drop t
+        $templine .= $line;
+    
+        // If it has a semicolon at the end, it's the end of the query
+        if (substr(trim($line), -1, 1) == ';') {
+            // Perform the query
+            $mbd->query($templine);
+            // Reset temp variable to empty
+            $templine = '';
+        }
+    }
+    
+    fclose($fp);
+    
+    echo "Database imported successfully";
+    
+
+
+
+
+    $mbd = null;
+} catch (PDOException $e) {
+    print "¡Error!: " . $e->getMessage() . "<br/>";
+    die();
+}
+?>
