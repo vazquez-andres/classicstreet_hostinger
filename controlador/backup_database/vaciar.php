749b46d3147f15cda1f85b7ca963d532b2bf8a99
andres.vazquez.ocampo.contractor <andres.vazquez.ocampo.contractor@bbva.com>
2022-12-03 23:38:25 -0600

    add new sql backup

--- controlador/backup_database/vaciar.php
+++ controlador/backup_database/vaciar.php
@@ -12,7 +12,7 @@ $host = "localhost";
 $nombre = "u917997591_classic_street";
 $usuario = "u917997591_andres";
 $password = "CSBS2022db";
-$fecha = date('Y-m-dh:i:s'); 
+$fecha = date('Y-m-dh-i-s'); 
 $nombre_sql = 'backup'.'_'.$fecha.'.sql';
 $dump = "mysqldump -h$host -u$usuario -p$password $nombre > $nombre_sql";
 exec ($dump);
