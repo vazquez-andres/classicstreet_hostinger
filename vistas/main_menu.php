568fb00323b37fbf857e0f6cd831d2843f80c76b
andres.vazquez.ocampo.contractor <andres.vazquez.ocampo.contractor@bbva.com>
2022-12-04 01:09:47 -0600

    add new backup database

--- vistas/main_menu.php
+++ vistas/main_menu.php
@@ -3,7 +3,7 @@
                                     <ul class="vertical-nav-menu">
                                         <li class="app-sidebar__heading">INICIO</li>
                                         <li>
-                                            <a href="index_admin.php" class="mm-active">
+                                            <a href="../../index_admin.php" class="mm-active">
                                                 <i class="metismenu-icon pe-7s-home"></i>
                                                 Dashboard
                                             </a>
@@ -11,7 +11,7 @@
                                         <li class="app-sidebar__heading">Ventas</li>
                                         <li>
 
-                                            <a href="ventas.php" >
+                                            <a href="../../ventas.php" >
                                                 <i class="metismenu-icon pe-7s-cart"></i>
                                                 Vender
                                             </a>
@@ -19,46 +19,46 @@
                                         <li class="app-sidebar__heading">Reportes</li>
                                         <li>
 
-                                            <a href="reportes.php" >
+                                            <a href="../../reportes.php" >
                                                 <i class="metismenu-icon pe-7s-id"></i>
                                                 Generar Reportes
                                             </a>
                                         </li>
                                         <li class="app-sidebar__heading">Usuarios</li>
                                         <li>
-                                            <a href="agregar_usuarios.php">
+                                            <a href="../../agregar_usuarios.php">
                                                 <i class="metismenu-icon pe-7s-add-user"></i>
                                                 Agregar Usuarios
                                             </a>
                                         </li>
                                         <li>
-                                            <a href="restablecer_password.php">
+                                            <a href="../../restablecer_password.php">
                                                 <i class="metismenu-icon pe-7s-key"></i>
                                                 Restablecer Password
                                             </a>
                                         </li>
                                         <li>
-                                            <a href="asignar_rol.php">
+                                            <a href="../../asignar_rol.php">
                                                 <i class="metismenu-icon pe-7s-tools"></i>
                                                 Asignar Rol
                                             </a>
                                         </li>                                        
                                         <li class="app-sidebar__heading">Stock</li>
                                         <li>
-                                            <a href="nuevo_stock.php">
+                                            <a href="../../nuevo_stock.php">
                                                 <i class="metismenu-icon pe-7s-upload"></i>
                                                 Agregar Stock
                                             </a>
                                         </li>
                                         <li>
-                                            <a href="modificar_stock.php">
+                                            <a href="../../modificar_stock.php">
                                                 <i class="metismenu-icon pe-7s-refresh"></i>
                                                 Modificar Stock
                                             </a>
                                         </li>
                                         <li class="app-sidebar__heading">SEGURIDAD</li>
                                         <li>
-                                            <a href="miHome.php">
+                                            <a href="../../miHome.php">
                                                 <i class="metismenu-icon pe-7s-camera"></i>
                                                 Xiaomi Mi Home App
                                             </a>
