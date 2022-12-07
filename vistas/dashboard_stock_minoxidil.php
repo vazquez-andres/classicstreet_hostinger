7058992a0ab3f1e507f8a4187c15075e2940f86a
andres.vazquez.ocampo.contractor <andres.vazquez.ocampo.contractor@bbva.com>
2022-12-06 23:11:53 -0600

    send mail to client

--- vistas/dashboard_stock_minoxidil.php
+++ vistas/dashboard_stock_minoxidil.php
@@ -17,9 +17,14 @@ if($rs['cantidad']<3){
     alert("El stock de minoxidil esta por terminarse");
     
     </script>';
-    $mensaje = "Le informamos que su stock de minoxidil está a punto de terminarse,\r\n le sugerimos contactar con el proveedor para evitar inconvenientes.";
+    date_default_timezone_set('America/Mexico_City');
+    $diassemana = array("Domingo","Lunes","Martes","Miercoles","Jueves","Viernes","Sábado");
+    $meses = array("Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre");
+    $dias = array("01","02","03","04","05","06","07","08","09","10","11","12","13","14","15","16","17","18","19","20","21","22","23","24","25","26","27","28","29","30","31");
+    $fecha_completa= $diassemana[date('w')]." ".$dias[date('d')-1]." de ".$meses[date('n')-1]. " del ".date('Y');
+    $mensaje = "Le informamos que su stock de minoxidil esta a punto de terminarse,\r le sugerimos contactar con el proveedor para evitar inconvenientes\n".$fecha_completa;
     $mensaje = wordwrap($mensaje, 70, "\r\n");
-    mail('andres_vazquezocampo@outlook.es', '¡AVISO EL STOCK DE MINOXIDIL ESTÁ POR TERMINARSE!', $mensaje);
+    mail('andres_vazquezocampo@outlook.es', '¡AVISO EL STOCK DE MINOXIDIL ESTA POR TERMINARSE!', $mensaje);
 }
 } catch (PDOException $e) {
 	echo 'PDOException : '.  $e->getMessage();
--- vistas/dashboard_stock_pomada.php
+++ vistas/dashboard_stock_pomada.php
@@ -16,9 +16,14 @@ try {
         echo'<script type="text/javascript">
         alert("El stock de pomada esta por terminarse");
         </script>';
-        $mensaje = "Le informamos que su stock de pomada está a punto de terminarse,\r\n le sugerimos contactar con el proveedor para evitar inconvenientes.";
+        date_default_timezone_set('America/Mexico_City');
+        $diassemana = array("Domingo","Lunes","Martes","Miercoles","Jueves","Viernes","Sábado");
+        $meses = array("Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre");
+        $dias = array("01","02","03","04","05","06","07","08","09","10","11","12","13","14","15","16","17","18","19","20","21","22","23","24","25","26","27","28","29","30","31");
+        $fecha_completa= $diassemana[date('w')]." ".$dias[date('d')-1]." de ".$meses[date('n')-1]. " del ".date('Y');
+        $mensaje = "Le informamos que su stock de pomada esta a punto de terminarse,\r\n le sugerimos contactar con el proveedor para evitar inconvenientes.\n".$fecha_completa;
         $mensaje = wordwrap($mensaje, 70, "\r\n");
-        mail('andres_vazquezocampo@outlook.es', '¡AVISO EL STOCK DE POMADA ESTÁ POR TERMINARSE!', $mensaje);
+        mail('andres_vazquezocampo@outlook.es', '¡AVISO EL STOCK DE POMADA ESTA POR TERMINARSE!', $mensaje);
             
     
     }
