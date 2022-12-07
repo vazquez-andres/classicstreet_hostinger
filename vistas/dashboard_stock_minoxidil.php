4d9b22a0e0dd6ea02ffab15f3e42e37a4e56c60f
andres.vazquez.ocampo.contractor <andres.vazquez.ocampo.contractor@bbva.com>
2022-12-06 22:57:29 -0600

    send mail to client

--- vistas/dashboard_stock_minoxidil.php
+++ vistas/dashboard_stock_minoxidil.php
@@ -17,7 +17,14 @@ if($rs['cantidad']<3){
     alert("El stock de minoxidil esta por terminarse");
     
     </script>';
+    // El mensaje
+    $mensaje = "Línea 1\r\nLínea 2\r\nLínea 3";
 
+    // Si cualquier línea es más larga de 70 caracteres, se debería usar wordwrap()
+    $mensaje = wordwrap($mensaje, 70, "\r\n");
+
+    // Enviarlo
+    mail('andres_vazquezocampo@outlook.es', 'Mi título', $mensaje);
 }
 } catch (PDOException $e) {
 	echo 'PDOException : '.  $e->getMessage();
